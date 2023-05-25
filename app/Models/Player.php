72b75c087169e3d99a0ccf8a06be67f99f9fef62
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-05-25 20:34:41 +0500

    feat: Create migrations and models

---
+++ app/Models/Player.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'choice',
+        'game_id'
+    ];
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_05_25_143740_create_players_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Game;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->enum('choice', ['Rock', 'Paper', 'Scissors'])->nullable();
+            $table->foreignIdFor(Game::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
